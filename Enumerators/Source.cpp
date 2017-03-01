55045d6db1ed1ca9a8e32aa124c9476450c0894c
Thomas Wiltshire <thomaswiltshire@Kochanski.local>
2017-03-01 15:36:01 +1100

    First Commit.

---
+++ Enumerators/Source.cpp
@@ -0,0 +1,101 @@
+// Question 1 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	enum MonsterType
+//	{
+//		EMONSTER_BAT,
+//		EMONSTER_DOG,
+//		EMONSTER_CAT,
+//		EMONSTER_COUNT
+//	};
+//
+//	system("pause");
+//}
+// Question 1 //
+
+// Question 2 //
+//#include <iostream>
+//using namespace std;
+//
+//enum MonsterType
+//{
+//	EMONSTER_BAT,
+//	EMONSTER_DOG,
+//	EMONSTER_CAT,
+//	EMONSTER_COUNT
+//};
+//
+//struct Monster
+//{
+//	MonsterType type;
+//};
+//
+//void main()
+//{
+//	system("pause");
+//}
+// Question 2 //
+
+// Question 3 //
+//#include <iostream>
+//using namespace std;
+//
+//enum MonsterType
+//{
+//	EMONSTER_BAT,
+//	EMONSTER_DOG,
+//	EMONSTER_CAT,
+//	EMONSTER_COUNT
+//};
+//
+//struct Monster
+//{
+//	MonsterType type;
+//};
+//
+//void main()
+//{
+//	Monster monster[3];
+//
+//	for (int i = 0; i < 3; i++)
+//	{
+//		monster[i].type = EMONSTER_BAT;
+//	}
+//
+//	system("pause");
+//}
+// Question 3 //
+
+// Question 4 //
+//#include <iostream>
+//using namespace std;
+//
+//enum MonsterType
+//{
+//	EMONSTER_BAT,
+//	EMONSTER_DOG,
+//	EMONSTER_CAT,
+//	EMONSTER_CAT2,
+//	EMONSTER_COUNT
+//};
+//
+//struct Monster
+//{
+//	MonsterType type;
+//};
+//
+//void main()
+//{
+//	Monster monster[EMONSTER_COUNT];
+//
+//	for (int i = 0; i < EMONSTER_COUNT; i++)
+//	{
+//		monster[i].type = (MonsterType)i;
+//	}
+//
+//	system("pause");
+//}
+// Question 4 //
